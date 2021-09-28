7162ab26c9b98a532f378f6df833c4f48a790976
Thomas Emerson <emersonvisuals@gmail.com>
2021-09-28 16:16:44 +1000

    wip mobile

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+
+// toggle footer 
+const all = document.querySelector('#all');
+const active = document.querySelector('#active');
+const completed = document.querySelector('#completed');
+
+all.addEventListener('click', function(){
+    all.classList.toggle('active');
+    active.classList.remove('active');
+    completed.classList.remove('active');
+});
+
+active.addEventListener('click', function(){
+    all.classList.remove('active');
+    active.classList.toggle('active');
+    completed.classList.remove('active');
+});
+
+completed.addEventListener('click', function(){
+    all.classList.remove('active');
+    active.classList.remove('active');
+    completed.classList.toggle('active');
+});
